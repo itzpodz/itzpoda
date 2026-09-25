b1347b3955754b987450531133eab3ed85e003c4
itzpodz <c.2112.podariu@gmail.com>
2026-09-25 17:24:03 +0200

    Update bot activity state description

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "itzpoda helper bot", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "built by itzpoda",     // this is what people actually see
         type: 0,               // Custom
       },
     ],
